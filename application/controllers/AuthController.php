ccd2ac67b8cf8bb73b78023e4e83acf51ada8cb4
Olga <mladi2010@yandex.ua>
2014-08-06 18:35:54 +0300

    ggg

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -278,7 +278,7 @@ class AuthController extends Zend_Controller_Action
 
         $client_id = '4485744 '; // ID приложения
         $client_secret = '9JYDwQ0AptHRMFRRr6ma'; // Защищённый ключ
-        $redirect_uri = 'http://moregeo.com//auth/regvk'; // Адрес сайта
+        $redirect_uri = 'http://moregeo.com/auth/regvk'; // Адрес сайта
         $url = 'http://oauth.vk.com/authorize';
 
         $params = array(
